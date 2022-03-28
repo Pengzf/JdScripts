c9db53b4f7ba1dd66b5fdf0e41cf7fd7f17b1527
ShuYu09 <805608215@qq.com>
2022-03-28 21:33:11 +0800

    修复逻辑

--- jdCookie.py
+++ jdCookie.py
@@ -5,8 +5,8 @@
 活动名称: 读取COOKIE / WSKEY
 Author: SheYu09
 '''
-import re
 from os import environ
+from re import compile
 from inspect import stack
 from time import strftime, localtime
 
@@ -19,6 +19,18 @@ def Name():
 	except:
 		print("自行添加环境变量：Name, 不同好友中间用&符号隔开\n")
 		exit()
+
+def re_key(r, e):
+	try:
+		return compile(rf'{r}').findall(e)
+	except:
+		pass
+
+def re_pin(r):
+	try:
+		return compile(r'pt_key=.*?;pt_pin=(.*?);').findall(r)[0]
+	except:
+		pass
 
 def JD_API_HOST(C):
 	try:
@@ -33,43 +45,20 @@ def JD_API_HOST(C):
 def COOKIE():
 	global ptkeyList, ptpinList
 	ck = JD_API_HOST(stack()[0][3])
-	ptkeyList = re.split('&|\n', ck) if ck else ''
-	ptpinList = re.compile(r'pin=(.*?);').findall(ck) if ck else ''
+	ptkeyList = re_key('pt_key=.*?;pt_pin=.*?;', ck)
+	ptpinList = re_key('pt_pin=(.*?);', ck)
 
 def WSKEY():
 	global wskeyList, wspinList
 	ck = JD_API_HOST(stack()[0][3])
-	wskeyList = re.split('&|\n', ck) if ck else ''
-	wspinList = re.compile(r'pin=(.*?);').findall(ck) if ck else ''
+	wskeyList = re_key('pin=.*?;wskey=.*?;', ck)
+	wspinList = re_key('pin=(.*?);', ck)
 
 def jdCookie():
-	COOKIE()
-	WSKEY()
-	if wskeyList and wspinList:
-		ckNumList = []
-		for i in wspinList:
-			try:
-				ckNum = ptpinList.index(i)
-				ckNumList.append(ckNum)
-			except:
-				pass
-		if ckNumList and ptkeyList:
-			for i in ckNumList:
-				ptkeyList.pop(i)
-				ptpinList.pop(i)
-	if ptkeyList and wskeyList:
-		cookiesList = wskeyList + ptkeyList
-		pinNameList = wspinList + ptpinList
-	elif ptkeyList and not wskeyList:
-		cookiesList = ptkeyList
-		pinNameList = ptpinList
-	elif not ptkeyList and wskeyList:
-		cookiesList = wskeyList
-		pinNameList = wspinList
-	else:
-		print("没有可用Cookie，已退出\n")
-		exit()
+	COOKIE(); WSKEY()
+	wskeyList and wspinList and [ptkeyList.remove(i) for i in [c for c in ptkeyList if re_pin(c) in wspinList]]
+	cookiesList = (ptkeyList and wskeyList and [wskeyList + ptkeyList] or ptkeyList and not wskeyList and [ptkeyList] or not ptkeyList and wskeyList and [wskeyList])[0]
 	print(f"====================共{len(cookiesList)}个京东账号Cookie=====================\n")
 	print(f"==================脚本执行- 北京时间(UTC+8)：{strftime('%Y-%m-%d %H:%M:%S', localtime())}==================\n")
-	return cookiesList, pinNameList
-
+	cookiesList or exit()
+	return cookiesList
